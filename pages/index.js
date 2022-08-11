43ec48c86e1d8d358e8426ac4615a0ca89579330
MiltonBarraza <miltonbarraza90@gmail.com>
2022-08-11 10:37:38 -0300

    Se le agrego las pages y header y layout

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import TarjetaContacto from "../components/TarjetaContacto";
+
+export default function Main() {
+  return (
+    <>
+      <h1>Hola from Home</h1>
+      <h1 id="duit360">Duit 360</h1>
+      <h1 id="servicios">Servicios</h1>
+      <h1 id="consejeros">Consejeros</h1>
+      <h1 id="contacto">Contacto</h1>
+      <TarjetaContacto/>
+    </>
+  )
+}
