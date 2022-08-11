ce11a5976f49b811f46124b9ffdb88ebf6bd81f1
MiltonBarraza <miltonbarraza90@gmail.com>
2022-08-11 10:44:28 -0300

    Se le agrego las pages y header y layout

---
+++ .next/server/pages/asistente.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/asistente";
+exports.ids = ["pages/asistente"];
+exports.modules = {
+
+/***/ "./pages/asistente.js":
+/*!****************************!*\
+  !*** ./pages/asistente.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Asistente)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction Asistente() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Hola from Asistente\"\n    }, void 0, false, {\n        fileName: \"D:\\\\Miltonete\\\\Programacion\\\\Pagina Levi\\\\Duit\\\\pages\\\\asistente.js\",\n        lineNumber: 3,\n        columnNumber: 5\n    }, this);\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9hc2lzdGVudGUuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFBZSxTQUFTQSxTQUFTLEdBQUc7SUFDbEMscUJBQ0UsOERBQUNDLElBQUU7a0JBQUMscUJBQW1COzs7OztZQUFLLENBQzdCO0NBQ0YiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9kdWl0LXJlcG8tbWlsdG9uLy4vcGFnZXMvYXNpc3RlbnRlLmpzPzE1MWQiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gQXNpc3RlbnRlKCkge1xyXG4gIHJldHVybiAoXHJcbiAgICA8aDE+SG9sYSBmcm9tIEFzaXN0ZW50ZTwvaDE+XHJcbiAgKVxyXG59Il0sIm5hbWVzIjpbIkFzaXN0ZW50ZSIsImgxIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/asistente.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/asistente.js"));
+module.exports = __webpack_exports__;
+
+})();
