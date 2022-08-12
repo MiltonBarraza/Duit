a78bf8747bfae4dca8491f50abb72b3903b38655
MiltonBarraza <miltonbarraza90@gmail.comgit config --global user.email miltonbarraza90@gmail.com>
2022-08-12 17:14:10 -0300

    se le agrego mediaquery al footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "174e8e92b6db8f3c"; }
+/******/ 		__webpack_require__.h = function() { return "08afd76bcdc2649e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
