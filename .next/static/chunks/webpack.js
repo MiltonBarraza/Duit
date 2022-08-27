3afd58f8c846ab06a6f4e4c776b7ea4cd0c33786
MiltonBarraza <miltonbarraza90@gmail.comgit config --global user.email miltonbarraza90@gmail.com>
2022-08-27 15:56:06 -0300

    Se agrego el Feature asistentes a la Page Asistente Personal, pero esta incompleta, falta pulir el CSS

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "174e8e92b6db8f3c"; }
+/******/ 		__webpack_require__.h = function() { return "37ab56d78318afcb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/Asistentes/index.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import styles from "../Asistentes/asistentes.module.scss";
+import { AsistentesImagenes } from "../asistentesComponentes";
+
+export default function Asistentes() {
+    return (
+        <section className={styles.asistentes}>
+
+            <div className={styles.tituloContainer}>
+
+                <h3>Elegí a tu</h3>
+                <div className={styles.containerImg}>
+                    <Image
+                        src={"/images/asistente-personal-color.png"}
+                        alt={"asistente-personal-color.png"}
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+            </div>
+
+            <div  >
+                <AsistentesImagenes />
+            </div>
+        </section>
+    )
+}
