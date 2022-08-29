6a72346d05460a318418c6edf2bc1e67447565b2
MiltonBarraza <miltonbarraza90@gmail.com>
2022-08-29 10:06:59 -0300

    Se trato de mejorar el Css

--- .next/static/webpack/webpack.26f471abf8704281.hot-update.js
+++ .next/static/webpack/webpack.26f471abf8704281.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "78ceec6883aae1e0"; }
+/******/ 	__webpack_require__.h = function() { return "7674c5e86a4897d5"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.77cbb0d32c194ab5.hot-update.js
+++ .next/static/webpack/webpack.77cbb0d32c194ab5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a6c8a099de4adb27"; }
+/******/ 	__webpack_require__.h = function() { return "26f471abf8704281"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
