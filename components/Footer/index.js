35d89b53186ff690ac71b3848259dc74ec36c92f
MiltonBarraza <miltonbarraza90@gmail.comgit config --global user.email miltonbarraza90@gmail.com>
2022-09-02 12:22:48 -0300

    Se actualizo el contenido para que sea mas adecuado a su version angular

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -8,39 +8,46 @@ export default function Footer() {
 
         <footer className={styles.footer}>
 
-            <div className={styles.footerCopy}>
+            <div className={styles.footerTexto}>
+
                 <p>Sabemos que digan lo que digan, no hay mejor lugar que ese donde podés escribir tu propia historia,
                     y por eso estamos con vos para darle la bienvenida a los mejores momentos que vas a vivir.</p>
+
             </div>
 
             <div className={styles.footerLogosContainer}>
 
                 <div className={styles.logoDuit}>
+
                     <Image
                         src="/images/duit_id_blanco_horizontal.png"
                         height={55}
-                        width={192}
+                        width={190}
                         alt={"duit_id_blanco_horizontal.png"}
                     />
+
                 </div>
+
                 <div className={styles.logoByHa}>
+
                     <Image
                         src="/images/byha.png"
                         height={25}
                         width={55}
                         alt={"byha.png"}
                     />
+
                 </div>
 
             </div>
 
-            <div className={styles.footerText}>
+            <div className={styles.footerTextoDatos}>
+
                 <p>San Luis 145, 1° Oficina H</p>
                 <p>Barrio Nueva Córdoba</p>
                 <p>Córdoba - Argentina</p>
-                <Link href={`tel:+${Contactos.whatsappDuit.numero}`}>
-                    <a >{Contactos.whatsappDuit.formateado}</a>
-                </Link>
+                <a href={`tel:+${Contactos.whatsappDuit.numero}`} >{Contactos.whatsappDuit.formateado}</a>
+                
             </div>
 
         </footer>
